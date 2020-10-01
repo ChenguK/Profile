67f3c5f63899d280a03c207bb72488b7d77e29d5
ChenguK <ChenguKargbo@gmail.com>
2020-10-01 13:28:20 -0400

    Finishing header buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import './Header.css';
 import Face from './face.png';
 import Envelope from './envelope.png';
 import Git from './github-logo.jpg';
+import LinkedIn from './linkedin.png';
 
 class Header extends Component {
 
@@ -19,19 +20,26 @@ class Header extends Component {
                             <br/>
                             <li className="container left-align">Chengu Kargbo is a natural problem solver, a creative Full Stack Web Developer, and an actor who loves helping creative people take their vision from an idea to a global audience.</li>
                             <br/>
-                            <li style={{ margin: "0 10rem 0 10rem", display: "inline"}}>
-                            <form  style={{ margin: "1rem", cursor: "pointer"}} className=" btn-floating btn-large green" 
+                            <li style={{ margin: "0 7rem 0 7rem", display: "inline"}}>
+                            <form   className=" btn-floating btn-large green" 
                                 action= "mailto:ChenguKargbo@gmail.com">
                                 <button 
                                 type="submit">
                                     <img id="header_email" width="45rem" style={{ marginTop: ".3rem", cursor: "pointer"}} src={ Envelope } alt="email"/>
                                 </button> 
                             </form>
-                            <form className=" btn-floating btn-large green" 
+                            <form style={{ margin: "2rem", cursor: "pointer"}} className=" btn-floating btn-large green" 
                                 action="https://github.com/ChenguK" target="_blank">
                                 <button 
                                 type="submit">
-                                    <img id="header_git" width="45rem" style={{ marginTop: ".3rem", cursor: "pointer"}} src={ Git } alt="GitHub"/>
+                                    <img id="header_git" width="45rem" style={{ marginTop: ".3rem", cursor: "pointer"}} src={ Git } alt="github"/>
+                                </button> 
+                            </form>
+                            <form  className=" btn-floating btn-large green" 
+                                action="https://www.linkedin.com/in/chengukargbo/" target="_blank">
+                                <button 
+                                type="submit">
+                                    <img id="header_linkedin" width="33rem" style={{ marginTop: ".8rem", cursor: "pointer"}} src={ LinkedIn } alt="linkedin"/>
                                 </button> 
                             </form>
                             </li>
